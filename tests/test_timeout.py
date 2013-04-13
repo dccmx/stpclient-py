a6d54ed7386efb2b2f768bb5c429d3213f1bae3e
dccmx <dccmx@dccmx.com>
2013-04-13 14:17:29 +0800

    fix timeout bug

--- tests/test_timeout.py
+++ tests/test_timeout.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
 # coding: utf-8
 import unittest
-from nose.tools import raises
+from nose.tools import raises, timed
 from stpclient.exceptions import STPTimeoutError
 
 
 class TestTimeout(unittest.TestCase):
     @raises(STPTimeoutError)
+    @timed(1.1)
     def test_timeout(self):
         from stpclient import Client
         client = Client('localhost', 9999, timeout=1)
         assert not client.closed
         client.call('long')
 
+    @timed(1.1)
     def test_timeout_state(self):
         from stpclient import Client
         from stpclient.exceptions import STPTimeoutError
